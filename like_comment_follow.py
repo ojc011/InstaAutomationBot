8bd89eae7617519bdda858a009d62c407779514e
Oliver C <ojc011@yahoo.com>
2022-07-12 15:17:14 -0500

    Defined comment function which pulls random comment str from array, clicks into the comment box using xpath, chooses random comment using send_keys, then clicks the post button are finding it using its xpath

--- like_comment_follow.py
+++ like_comment_follow.py
@@ -3,25 +3,54 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import random
+from random import randint
 import pyautogui
 
 def like_with_hashtags(driver):
-    random_number = random.randint(2, 5)
-    hashtags = ["motivation", "fitness"]
+    hashtags = ["fitness", "motivation"]
     for i in hashtags:
-        comploted_likes = 0
+        completed_likes = 0
         wait = WebDriverWait(driver, 10)
         url = "https://www.instagram.com/explore/tags/"+i+"/"
         driver.get(url)
         path = "/html/body/div[1]/div/div[1]/div/div[1]/div/div/div/div[1]/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div[1]/div[2]"
         first_photo = wait.until(EC.element_to_be_clickable((By.XPATH,path)))
         first_photo.click()
-        for i in range(1,20):
+        for i in range(1,100):
             path = "/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/button"
             like_button = wait.until(EC.element_to_be_clickable((By.XPATH,path)))
             like_button.click()
             pyautogui.press('right') 
-            time.sleep(random_number)
-            comploted_likes+=1
+            time.sleep(randint(2, 5))
+            completed_likes+=1
+
+def only_comments(driver):
+    comments = ["Looking great", "Awesome work", "Keep it up", "Nice!"]
+    hashtags = ["fitness", "motivation"]
+    for i in hashtags:
+        completed_comments = 0
+        wait = WebDriverWait(driver, 10)
+        url = "https://www.instagram.com/explore/tags/"+i+"/"
+        driver.get(url)
+        path = "/html/body/div[1]/div/div[1]/div/div[1]/div/div/div/div[1]/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div[1]/div[2]"
+        first_photo = wait.until(EC.element_to_be_clickable((By.XPATH,path)))
+        first_photo.click()
+        for i in range(1,100):
+            path = "/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form"
+            comment_area = wait.until(EC.element_to_be_clickable((By.XPATH,path)))
+            comment_area.click()
+            path = "/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/textarea"
+            time.sleep(3)
+            comment_text = wait.until(EC.presence_of_element_located((By.XPATH,path)))
+            comment = comments[randint(0,len(comments)-1)]  
+            comment_text.send_keys(comment)
+            time.sleep(3)
+            path = "/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/button"
+            post_button = wait.until(EC.element_to_be_clickable((By.XPATH,path)))
+            post_button.click()
+            pyautogui.press('right') 
+            time.sleep(randint(2,5))
+            completed_comments+=1
+    print("Completed comments : "+ completed_comments)
+
             
--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import os
 import time
 from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from login import login_func
-from like_comment_follow import like_with_hashtags
+from like_comment_follow import like_with_hashtags, only_comments
+from random import randint
+import pyautogui
+
 load_dotenv()
 username = os.getenv('IG_UN') ## ENTER YOUR IG USERNAME HERE ##
 password = os.getenv('IG_PW') ## ENTER YOUR IG PASSWORD HERE ##
@@ -13,6 +18,6 @@ driver = webdriver.Chrome("C:/Program Files/chromedriver/chromedriver.exe")
 url = "https://www.instagram.com/"
 driver.get(url)
 time.sleep(2)
-wait = WebDriverWait(driver, 10)
 login_func(username,password,driver)
-like_with_hashtags(driver)
+wait = WebDriverWait(driver, 10)
+only_comments(driver)
