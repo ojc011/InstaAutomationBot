6c1de8c02bf93000a09b407663ace19a85f125ef
Oliver C <ojc011@yahoo.com>
2022-07-13 13:28:57 -0500

    defined unfollow_with_file which reads the not_following_back txt file, takes each name as [i] and opens there instagram page, clicks unfollow twice, and proceeds to next instance of i.

--- followers_following.py
+++ followers_following.py
@@ -3,6 +3,7 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from random import randint
 
 def get_follower(driver,profilename):
     time.sleep(1)
@@ -79,3 +80,35 @@ def get_following_names(driver,profilename):
         f.write(i)
         f.write("\n")
     return name_of_following   
+
+def names_not_following_back(followers_file,following_file):
+    f = open(followers_file,"r")
+    follower_names = f.readlines()
+    f = open(following_file,"r")
+    following_names = f.readlines()
+    not_following_back = []
+    for i in following_names:
+        if i not in follower_names and i !="":
+            not_following_back.append(i[:-1])
+    f = open("not_following_back.txt","w")
+    for i in not_following_back:
+        f.write(i)
+        f.write("\n")
+        
+def unfollow_with_file(driver,filename):
+    f = open(filename,"r")
+    lines = f.readlines()
+    not_following_back = []
+    for i in lines:
+        not_following_back.append(i[:-1])
+    wait = WebDriverWait(driver,10)
+    for i in not_following_back:
+        url = "https://www.instagram.com/"+i+"/"
+        driver.get(url)
+        path = "/html/body/div[1]/div/div[1]/div/div[1]/div/div/div/div[1]/div[1]/section/main/div/header/section/div[1]/div[1]/div/div[2]/button"
+        unfollow1 = wait.until(EC.element_to_be_clickable((By.XPATH,path)))
+        unfollow1.click()
+        time.sleep(randint(1,3))
+        path = "/html/body/div[1]/div/div[1]/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[3]/button[1]"
+        unfollow2 = wait.until(EC.element_to_be_clickable((By.XPATH,path)))
+        unfollow2.click()
--- main.py
+++ main.py
@@ -7,9 +7,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from login import login_func
-from like_comment_follow import like_with_hashtags, only_comments, follow_with_hashtags, like_comment_follow
-from followers_following import get_following_names, get_followers_names
-from random import randint
+from like_comment_follow import *
+from followers_following import *
 
 load_dotenv()
 username = os.getenv('IG_UN') ## ENTER YOUR IG USERNAME HERE ##
@@ -18,23 +17,11 @@ driver = webdriver.Chrome("C:/Program Files/chromedriver/chromedriver.exe")
 url = "https://www.instagram.com/"
 driver.get(url)
 time.sleep(2)
-#login_func(username,password,driver)
+login_func(username,password,driver)
 wait = WebDriverWait(driver, 10)
 profilename = "oliver.cronk"
 
-def names_not_following_back(followers_file,following_file):
-    f = open(followers_file,"r")
-    follower_names = f.readlines()
-    f = open(following_file,"r")
-    following_names = f.readlines()
-    not_following_back = []
-    for i in following_names:
-        if i not in follower_names and i !="":
-            not_following_back.append(i[:-1])
-    f = open("not_following_back.txt","w")
-    for i in not_following_back:
-        f.write(i)
-        f.write("\n")
-        
-names_not_following_back("follower_names.txt", "following_names.txt")
+
+filename = "not_following_back.txt"
+unfollow_with_file(driver,filename)
 
